6d1dac88e78beb353e4df3c6f819215690af8288
Aleks Borovtsov <aBorovtsov1994@users.noreply.github.com>
2021-10-25 16:53:26 -0400

    Created check_input_int function to check for proper input. Formatted file for readability.

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ num_of_users = 0
 bill_subtotal = 0
 tips_amount = 0
 fees_amount = 0
+num_of_users_str = "Please enter the amount of users for this bill.\n"
 
 
-def check_input_int(user_input):
+def check_input_int(inquiry_string):
     while True:
+        user_input = input(inquiry_string)
         if user_input.isnumeric():
             return user_input
         else:
@@ -18,8 +20,7 @@ def check_input_int(user_input):
 
 
 print("Hello and welcome to my Alpha calculator! Please go ahead and enter the subtotal!")
-
-num_of_users = check_input_int(input("Please enter the amount of users for this bill.\n"))
+num_of_users = check_input_int()
 
 if __name__ == '__main__':
     print("The main program has run!")
